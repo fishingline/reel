b95a041bc86b48b633bb2eb70c2ff084726f716a
mattmc3 <mattmc3@gmail.com>
2021-11-26 12:45:04 -0500

    Fix plugin-load function

--- functions/plugin-load.fish
+++ functions/plugin-load.fish
@@ -1,11 +1,12 @@
 function plugin-load -a plugin_dir -d "Load a plugin"
-    if ! test -d "$plugin_dir"
+    if not test -d "$plugin_dir"
         echo >&2 "plugin-load: Plugin directory not found: '$plugin_dir'."
         return 1
     end
-    test -d $plugin_dir/completions && set $fish_complete_path[1] $plugin_dir/completions $fish_complete_path[2..-1]
-    test -d $plugin_dir/functions && set $fish_function_path[1] $plugin_dir/functions $fish_function_path[2..-1]
-    for file in $plugin_dir/conf.d/*
-        builtin source "$f"
+    test -d $plugin_dir/completions; and set fish_complete_path $fish_complete_path[1] $plugin_dir/completions $fish_complete_path[2..-1]
+    test -d $plugin_dir/functions; and set fish_function_path $fish_function_path[1] $plugin_dir/functions $fish_function_path[2..-1]
+    test -f $plugin_dir/config.fish; and builtin source "$plugin_dir/config.fish"
+    for conffile in $plugin_dir/conf.d/*
+        builtin source "$conffile"
     end
 end
