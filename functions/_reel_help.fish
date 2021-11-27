f18c5a8661a42808bdefbf58484f4a61f1d7d51e
mattmc3 <mattmc3@gmail.com>
2021-11-26 21:57:12 -0500

    Add help and version functions

---
+++ functions/_reel_help.fish
@@ -0,0 +1,18 @@
+function _reel_help \
+    --description "Print out reel help information"
+
+    echo "reel - Reel in your fish plugins"
+    echo ""
+    echo "Usage:"
+    echo "  reel [-h|--help]"
+    echo "  reel [-v|--version]"
+    echo "  reel <command> [<args>...]"
+    echo ""
+    echo "Commands:"
+    echo ""
+    echo "  reel in <plugins...>    Install plugin(s)"
+    echo "  reel rm <plugins...>    Remove plugin(s)"
+    echo "  reel up [<plugins...>]  Update plugin(s)"
+    echo "  reel ls [--style=xxx]   List plugins (styles: name, repo, path, url)"
+    echo "  reel on                 Enable all plugins (usually in fish.config)"
+end
---
+++ functions/_reel_version.fish
@@ -0,0 +1,5 @@
+function _reel_version \
+    --description "Reel version information"
+
+    echo "reel, version $reel_version"
+end
